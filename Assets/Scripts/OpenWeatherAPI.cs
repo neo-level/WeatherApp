1088404185be16a571dadfcb50b3d3a3bbfd7caa
neo-level <info@neo-level.com>
2021-12-03 21:35:49 +0100

    Calling the API

--- Assets/Scripts/OpenWeatherAPI.cs
+++ Assets/Scripts/OpenWeatherAPI.cs
@@ -1,18 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Web;
 using UnityEngine;
+using UnityEngine.Networking;
+
 
 public class OpenWeatherAPI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static readonly string APIBaseUrl = "https://api.openweathermap.org/data/2.5/forecast/weather?q={0}&cnt=5&appid={1}";
 
-    // Update is called once per frame
-    void Update()
+    [Tooltip("The API key that allows access to the OpenWeatherMap API")]
+    public string apiKey;
+    
+    public ResponseContainer Response { get; private set; }
+
+    public IEnumerator GetForecast(string city)
     {
-        
+        Response = null;
+
+        string UrlEncodedCity = HttpUtility.UrlEncode(city);
+        string url = string.Format(APIBaseUrl, UrlEncodedCity, apiKey);
+
+        var webRequest = UnityWebRequest.Get(url);
+
+        yield return webRequest.SendWebRequest();
+        if (webRequest.result == UnityWebRequest.Result.Success)
+        {
+            string json = webRequest.downloadHandler.text;
+            Response = JsonUtility.FromJson<ResponseContainer>(json);
+        }
+        else
+        {
+            Debug.Log(webRequest.error);
+        }
     }
 }
