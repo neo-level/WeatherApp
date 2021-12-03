9dd51a573273ecbb94a738f7f079a4c8a0efbeb8
neo-level <info@neo-level.com>
2021-12-03 22:11:43 +0100

    data model for the DayCardModel.cs

--- Assets/Scripts/DayCardModel.cs
+++ Assets/Scripts/DayCardModel.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class DayCardModel : MonoBehaviour
+/// <summary>
+/// This class is used to create a friendly data model for the DayCard script.
+/// </summary>
+class DayCardModel
 {
-    // Start is called before the first frame update
-    void Start()
+    public float MinimumTemp { get; }
+
+    public float MaximumTemp { get; }
+
+    public string Day { get; }
+
+    public string Date { get; }
+
+    public Sprite Icon { get; }
+
+    public string Header { get; }
+
+    public string Details { get; }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="item">Response item</param>
+    /// <param name="icon">Sprite</param>
+    public DayCardModel(ResponseItem item, Sprite icon)
     {
-        
+        MaximumTemp = item.temperature.maximum;
+        MinimumTemp = item.temperature.minimum;
+
+        var date = UnixTimeToDateTime(item.daytime);
+        Day = date.DayOfWeek.ToString();
+        Date = $"{date.Month}/{date.Day}";
+        Header = item.weather[0].main;
+        Details = item.weather[0].description;
+        Icon = icon;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Convert from UNIX time to date time.
+    /// </summary>
+    /// <param name="unixTime">Unix time (seconds since Epoch)</param>
+    /// <returns>Date and time</returns>
+    private DateTime UnixTimeToDateTime(long unixTime)
     {
-        
+        var time = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        time = time.AddSeconds(unixTime).ToLocalTime();
+        return time;
     }
 }
