26c8693c880fd052e7b5248406d04dbb6d3334e0
neo-level <info@neo-level.com>
2021-12-03 22:15:28 +0100

    created event listener on button to convert fahrenheit to celsius and vice versa.

--- Assets/Scripts/ChangeScale.cs
+++ Assets/Scripts/ChangeScale.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Change scale from celsius to fahrenheit and vice versa.
+/// </summary>
 public class ChangeScale : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool _showMetric = true;
 
-    // Update is called once per frame
-    void Update()
+    public Sprite celsius;
+    public Sprite fahrenheit;
+
+    public Image image;
+    public DayCard[] days;
+    
+    private void Awake()
     {
-        
+        // Attach a listener to the click event of the button this script is 
+        // attached. When clicked, the button will negate the show metric flag
+        // and inform all the DayCard instances to show the correct temperature
+        // scale.
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(delegate
+        {
+            _showMetric = !_showMetric;
+            foreach (var day in days)
+            {
+                day.ShowMetric(_showMetric);
+            }
+
+            image.sprite = _showMetric ? celsius : fahrenheit;
+        });
     }
 }
